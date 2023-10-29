97079d30f993b0e796bd8dbf69690ee78c2d6966
AMEERHAMZA123123 <ameer.hamzaarain01@gmail.com>
2023-10-29 13:21:11 +0500

    aaaa

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MovieList from './Movielist';
 function App() {
   return (
     <div className="App">
-      <h1>TMDb Movie App</h1>
+      <h1>TMDb Mp</h1>
       <MovieList />
     </div>
   );
